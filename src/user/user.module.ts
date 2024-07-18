4694086d892f6e4e29a51a68fdf8b81ff15fcb31
Julien TANGUY <tanguyjulien@hotmail.fr>
2024-07-18 15:29:00 +0200

    init pipeline devops

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,15 +2,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PrismaService } from 'prisma/prisma.service';
 import { ProfileService } from 'src/user/profile.service';
+import { ResetToken, ResetTokenSchema } from './resetToken.schema';
 import { UserController } from './user.controller';
 import { User, UserSchema } from './user.schema';
 import { UserService } from './user.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ResetToken, ResetTokenSchema } from './resetToken.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema },{ name: ResetToken.name, schema: ResetTokenSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: ResetToken.name, schema: ResetTokenSchema },
+    ]),
     // ClientsModule.register([
     //   {
     //     name: 'NATS',
